c479b695221751c6aa3688a4360c8bc6423da53f
rlubaschewski <rlubaschewski@liveperson.com>
2018-11-12 14:14:12 +0100

    erste aufgabe hinzugefügt

---
+++ src/FastaUtilizer.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FastaUtilizer {
+
+    public static List<String> getArrayFromFasta(String path) {
+        List<String> fasta = new LinkedList<>();
+        String fastaBlock = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            for (String line; (line = br.readLine()) != null; ) {
+                if (line.charAt(0) != '>') {
+                    fastaBlock += line;
+                } else {
+                    if (fastaBlock.length() != 0) {
+                        fasta.add(fastaBlock);
+                    }
+                    fastaBlock = "";
+                }
+            }
+            fasta.add(fastaBlock);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fasta;
+    }
+
+    public static void writeFastaFile(String path, String[] comments, String[] contents) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            for (int i = 0; i < comments.length; i++) {
+                writer.write(">");
+                writer.write(comments[i]);
+                for (int j = 0; j < contents[i].length(); j++) {
+                    if (j % 59 == 0) {
+                        writer.newLine();
+                    }
+                    writer.write(contents[i].charAt(j));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/SequenceTools.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SequenceTools {
+
+    private static final String filePath = "/Users/rlubaschewski/Desktop/uni/BIM/assets/fasta/NR1H4-BIM-Uebung.fasta";
+
+    public static String getMatureMRNA(String sequence, String[] exons, boolean keepFirstIntron) {
+        String matureMRNA = "";
+        List<String> introns = new LinkedList<>();
+        int lastExonIndex = 0;
+        for (int i = 0; i < exons.length; i++) {
+            int exonIndex = sequence.indexOf(exons[i]);
+            if (exonIndex != 1) {
+                if (lastExonIndex != 0 && lastExonIndex < exonIndex) {
+                    introns.add(sequence.substring(lastExonIndex + 1, exonIndex - 1));
+                }
+                lastExonIndex = sequence.lastIndexOf(exons[i]);
+                if (keepFirstIntron && introns.size() == 1) {
+                    matureMRNA += introns.get(0);
+                }
+                matureMRNA += exons[i];
+            }
+        }
+        StringBuilder builder = new StringBuilder(matureMRNA);
+        for (int i = 0; i < builder.length(); i++) {
+            if (builder.charAt(i) == 'A') {
+                builder.setCharAt(i, 'U');
+            } else if (builder.charAt(i) == 'T') {
+                builder.setCharAt(i, 'A');
+            } else if (builder.charAt(i) == 'C') {
+                builder.setCharAt(i, 'G');
+            } else if (builder.charAt(i) == 'G') {
+                builder.setCharAt(i, 'C');
+            }
+        }
+        return builder.toString();
+    }
+
+    public static String mRNAtoProtein(String mrna, boolean log) {
+        String protein = "";
+        String proteinStartToStop = "";
+        int start = 0, stop = 0;
+        for (int i = 0; i < mrna.length() - 2; i++) {
+            protein += CodonTranslater.codon2aa(mrna.substring(i, i + 3));
+        }
+        for (int i = 0; i < protein.length(); i++) {
+            if (start > 0 && stop > 0 && stop > start) {
+                proteinStartToStop = protein.substring(start, stop);
+                break;
+            }
+            if (protein.charAt(i) == 'M') {
+                if (log)
+                    System.out.println("The 5' untranslated region of the mRNA starts at index 0 and ends at index " + (i - 1));
+                start = i;
+            } else if (protein.charAt(i) == '*') {
+                stop = i;
+                if (stop > start && start > 0 && log) {
+                    System.out.println("The 3' untranslated region of the mRNA starts at index " + i + " and ends at index " + protein.length());
+                }
+            }
+        }
+        return proteinStartToStop;
+    }
+
+    public static void main(String[] args) {
+        List<String> fasta = FastaUtilizer.getArrayFromFasta(filePath);
+        String sequence = fasta.get(0);
+        String[] exons = new String[fasta.size() - 1];
+        for (int i = 1; i < fasta.size(); i++) {
+            exons[i - 1] = fasta.get(i);
+        }
+        String matureMRNA = getMatureMRNA(sequence, exons, false);
+        String[] commentsRNA = {"mature mRNA of NR1H4"};
+        String[] contentsRNA = {matureMRNA};
+        FastaUtilizer.writeFastaFile("/Users/rlubaschewski/Desktop/uni/BIM/assets/fasta/NR1H4-reife-mRNA.fasta", commentsRNA, contentsRNA);
+        String protein = mRNAtoProtein(matureMRNA, true);
+        String[] commentsProtein = {"Primary Protein Sequence of NR1H4"};
+        String[] contentsProtein = {protein};
+        FastaUtilizer.writeFastaFile("/Users/rlubaschewski/Desktop/uni/BIM/assets/fasta/NR1H4-Protein.fasta", commentsProtein, contentsProtein);
+        String mRNAWithFirstIntron = getMatureMRNA(sequence, exons, true);
+        String proteinWithFirstIntron = mRNAtoProtein(mRNAWithFirstIntron, false);
+        String[] commentsRNAWithIntron = {"Protein with first Intron kept"};
+        String[] contentsRNAWithIntron = {proteinWithFirstIntron};
+        FastaUtilizer.writeFastaFile("/Users/rlubaschewski/Desktop/uni/BIM/assets/fasta/NR1H4-Protein-Variante.fasta", commentsRNAWithIntron, contentsRNAWithIntron);
+    }
+
+}
